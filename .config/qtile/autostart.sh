932a68eae50ba10b218131ffb2bc5585a5b9baf1
Val Blaze <leoswhited@gmail.com>
2023-10-30 15:42:53 -0500

    removed xrdb and added configuration to picom

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --mode 1920x1080 --rate 144
-feh --bg-scale ~/Pictures/wallpapers/katana.png &
-picom &
+xrandr --output DisplayPort-0 --mode 1920x1080 --rate 144 &
+feh --bg-scale ~/Pictures/blue-firewatch.jpg &
+picom -b --config ~/.config/picom.conf 
+dunst &
