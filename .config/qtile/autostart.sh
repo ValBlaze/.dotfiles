6aeda6dd85942b4d1aed5ec4a54d11742e99730a
Val Blaze <leoswhited@gmail.com>
2023-11-04 21:18:42 -0500

    added nm-applet to startup

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -3,3 +3,4 @@ xrandr --output DisplayPort-0 --mode 1920x1080 --rate 144 &
 feh --bg-scale ~/Pictures/blue-firewatch.jpg &
 picom -b --config ~/.config/picom.conf 
 dunst &
+nm-applet --sm-disable &
