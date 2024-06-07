d6fdd526a6173f2bcae7a617fa29fd10255f6340
Val Blaze <lorenzosc@skiff.com>
2024-06-07 18:38:51 -0500

    dream nvim

--- .config/nvim/lua/plugins/configs/treesitter.lua
+++ .config/nvim/lua/plugins/configs/treesitter.lua
@@ -1,12 +1,12 @@
-local options = {
-  ensure_installed = { "lua" },
-
+return {
+  ensure_installed = {
+    "go",
+    "lua",
+    "rust",
+  },
   highlight = {
     enable = true,
     use_languagetree = true,
   },
-
   indent = { enable = true },
 }
-
-return options
